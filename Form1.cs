35219c13fc20c628d9df4d5f50a7e7ed46fbb2e5
Operavevo <simon.b.gold@gmail.com>
2016-07-12 22:03:33 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scammer_Bingo_Reborn
+{
+
+
+    public partial class Form1 : Form
+    {
+       // int halfway = 0;
+       // int done = 0;
+
+
+        private static int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutSBRToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 about = new AboutBox1();
+            about.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button13.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttonClick(sender);
+        }
+
+        private void buttonClick(object sender)
+        {
+            score++;
+            string newScore = score + "/12";
+            label3.Text = newScore;
+            ((Button)sender).Enabled = false;
+            button13.Focus();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+            button10.Enabled = true;
+            button11.Enabled = true;
+            button12.Enabled = true;
+            score = 0;
+            label3.Text = "0/12";
+            button13.Focus();
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int halfway = 0;
+            int done = 0;
+
+
+
+            if (score == 6)
+            {
+
+                timer1.Enabled = false;
+                MessageBox.Show("(placeholder text for halfway point)");
+                
+            }
+                
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (score == 12)
+            {
+
+                timer2.Enabled = false;
+                MessageBox.Show("(placeholder text for victory)");
+
+            }
+
+        }
+    }
+}
